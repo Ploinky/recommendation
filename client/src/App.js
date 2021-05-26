1c182adc85d69e99b5123c3c4896e0498a30dbe9
Joba <mail@johannes-litger.de>
2021-05-26 20:19:25 +0100

    Fixed issues with RecommendationList component

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
         <div>
         </div>
         <Switch>
-          <Route path="/:id" children={<RecommendationList />}/>
+          <Route path="/:id" component={RecommendationList}/>
         </Switch>
       </Router>
 
--- client/src/RecommendationList.js
+++ client/src/RecommendationList.js
@@ -2,34 +2,45 @@ import React, { Component } from 'react';
 import { Recommendation } from './Recommendation'
 
 export class RecommendationList extends Component {    
-    
-    count = 0;
-    id = 0;
-    date = [];
-    
-    render() {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [],
+            count: 0,
+            id: 0
+        }
+    }
 
-            fetch('http://localhost:5001/recommendations/' + this.id)
-            .then(res => { 
-                console.log('Response : ' + JSON.stringify(res))
-                return res.json() 
-            })
-            .then((data) => {
-                console.log('result : ' + JSON.stringify(data));
-                this.count = data.length
-                this.data = data
-            });
-        
-        
+    componentDidMount() {
+        this.state.id = this.props.match.params.id;
+      
+      fetch('http://localhost:5001/recommendations/' + this.state.id)
+      .then(res => {
+          return res.json() 
+      })
+      .then((data) => {
+          console.log('result : ' + JSON.stringify(data));
+          this.setState({count: data.length,
+            data: data
+        })
+      });
+      
+  }
+    
+    render() {  
         return (
+
             <div>
-            <h3>ID: {this.id}</h3>
-            <p>Anzahl Recommendation: {this.ount}</p>
-            { this.data.map((recommendation) => {
-                console.log(JSON.stringify(recommendation))
-                return (<Recommendation key={recommendation.recoid} title={recommendation.title} link={recommendation.link} />)
-            })
-        }  
-        </div> 
-    )}
+            <h3>ID: {this.state.id}</h3>
+            <p>Anzahl Recommendation: {this.state.count}</p>
+            {console.log(this.state.data)}
+            {
+                this.state.data.map((recommendation) => {
+                    console.log("recommendation:" + JSON.stringify(recommendation))
+                    return (<Recommendation key={recommendation.recoid} title={recommendation.title} link={recommendation.link} />)
+                })
+            }  
+            </div> 
+        )
+    }
 }
