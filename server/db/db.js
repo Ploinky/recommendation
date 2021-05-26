622ac7a24875bd5ecbf52ddac3b54eb694724298
Joba <mail@johannes-litger.de>
2021-05-26 18:22:30 +0100

    added log when connecting to database

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/recomapp', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb://localhost:27017/recomapp', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true})
+    .then(() => {
+        console.log('Connection to local mongodb established')
+    });
